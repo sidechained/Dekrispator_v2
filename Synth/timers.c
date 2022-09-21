0858aa0b668ba4b81c69e057db0e4808eb221032
Graham Booth <graham.r.booth@gmail.com>
2022-09-21 22:18:29 +0200

    tidied timers.c/.h

--- Synth/timers.c
+++ Synth/timers.c
@@ -1,20 +1,10 @@
-/**
- ******************************************************************************
- * File Name		: timers.c
- * Date				:
- * Author			: Xavier Halgand
- * Description		:
- ******************************************************************************
- */
-
-/* Includes ------------------------------------------------------------------*/
+// Author: Xavier Halgand
 
 #include "timers.h"
 
 #define CHECK_MSEC 		4 // Read hardware every x msec
 #define PRESS_MSEC 		20 // Stable time before registering pressed
 #define RELEASE_MSEC 	20 // Stable time before registering released
-
 
 static uint32_t TimingDelay = 0;
 static uint32_t TimingDelay1 = 0;
@@ -25,20 +15,11 @@ static bool 	DebouncedKeyPress = false; // This holds the debounced state of the
 static bool 	Key_changed = false;
 static bool 	Key_pressed = false;
 
-/*----------------------------------------------------------------------------------------------*/
-
 extern void ButtonPressed_action(void);
 extern void ButtonReleased_action(void);
 
-//---------------------------------------------------------------------------
+// Decrements the TimingDelays variables. Called by SysTick_Handler() in stm32f4xx_it.c
 
-
-/**
- * @brief  Decrements the TimingDelays variables.
- * 			Called by SysTick_Handler() in stm32f4xx_it.c
- * @param  None
- * @retval None
- */
 void TimingDelay_Decrement(void)
 {
 	if (TimingDelay != 0)
@@ -51,8 +32,6 @@ void TimingDelay_Decrement(void)
 	}
 }
 
-//---------------------------------------------------------------------------
-
 uint32_t getTime(void)
 {
 	return TimingDelay1;
@@ -62,34 +41,22 @@ void setTime(uint32_t val)
 {
 	TimingDelay1 = val;
 }
-/***********************************************************************************/
-/**
- * @brief  Turns selected LED On for  a given duration.
- * @param  Led: Specifies the Led to be set on.
- *   This parameter can be one of following parameters:
- *     @arg LED4
- *     @arg LED3
- *     @arg LED5
- *     @arg LED6
- * @retval None
- */
+
+// Turns selected LED On for a given duration.
 void start_LED_On(Led_TypeDef Led, uint32_t duration)
 {
 	BSP_LED_On(Led);
 	LED_counter[Led] = duration;
 }
-/*------------------------------------------------------------------------------------*/
-/* Function called by SysTick_Handler()  */
+
+// Function called by SysTick_Handler()
 void update_temporized_LED(Led_TypeDef Led)
 {
 	LED_counter[Led]--;
 	if (LED_counter[Led] == 0) BSP_LED_Off(Led);
 }
 
-/***************************************************************************************************************************/
-// Service routines called every CHECK_MSEC to
-// debounce both edges
-
+// Service routines called every CHECK_MSEC to debounce both edges
 void DebounceUserButton(void) // Called by SysTick_Handler() in file stm32f4xx_it.c
 {
 	bool RawState;
--- Synth/timers.h
+++ Synth/timers.h
@@ -1,17 +1,8 @@
-/**
- ******************************************************************************
- * File Name		: timers.h
- * Author			: Xavier Halgand
- * Date				:
- * Description		:
- ******************************************************************************
- */
+// Author: Xavier Halgand
 
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __TIMERS_H
 #define __TIMERS_H
 
-/* Includes ------------------------------------------------------------------*/
 #include <audio_conf.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -20,7 +11,6 @@
 #include "stm32f4_discovery.h"
 #include "soundGen.h"
 
-/* Exported functions ------------------------------------------------------- */
 void 		TimingDelay_Decrement(void);
 void 		setSysTick(void);
 uint32_t 	getTime(void);
@@ -29,5 +19,4 @@ void 		start_LED_On(Led_TypeDef Led, uint32_t duration);
 void 		update_temporized_LED(Led_TypeDef Led);
 void 		DebounceUserButton(void);
 
-/*************************************************************************************/
-#endif /* __TIMERS_H */
+#endif
